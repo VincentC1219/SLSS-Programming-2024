f9e02b55afb8535e68167b1c1f00ab669cc9ea13
Vincent Chen <vincentc0911@gmail.com>
2024-04-04 12:15:59 -0700

    Added more morefunction notes

--- Notes/notes-2-2-morefunctions.py
+++ Notes/notes-2-2-morefunctions.py
@@ -2,3 +2,29 @@
 # Vincent Chen
 # April 4, 2024
 
+
+
+def star(num_star: int) -> str:
+    # Return given number of stars *****
+    value = " "
+    # if value == 0 or value == 1 "*"
+    # if value > 1 "8" * mum_star
+    # else negative number -> error
+    if num_star == 0 or num_star == 1:
+        value = "*"
+    return value
+
+
+
+
+
+
+# # multiply strings
+# greeting = "Hello"
+# print(greeting * 2)
+
+
+# print("The quick brown fox jumped over the lazy dog. " * 2)
+
+print(star(1))
+print(star(100))
