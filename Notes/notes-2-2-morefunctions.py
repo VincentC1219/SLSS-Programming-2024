44a4d3594f04fedc3271243c28410cf8e5243798
Vincent Chen <vincentc0911@gmail.com>
2024-04-05 14:56:20 -0700

    Added new function

--- Notes/notes-2-2-morefunctions.py
+++ Notes/notes-2-2-morefunctions.py
@@ -10,12 +10,13 @@ def star(num_star: int) -> str:
     # if value == 0 or value == 1 "*"
     # if value > 1 "8" * mum_star
     # else negative number -> error
-    if num_star == 0 or num_star == 1:
+    if num_star < 0:
+        print("Sorry, Can't do that")
+    elif num_star > 0:
+        value = "*" * num_star
+    elif num_star == 0 or num_star == 1:
         value = "*"
     return value
-
-
-
 
 
 
@@ -28,3 +29,16 @@ def star(num_star: int) -> str:
 
 print(star(1))
 print(star(100))
+
+
+def pyramid(base_width: int):
+    # Prints a pyramid of stars of given base width
+    # Params:
+    #   Base_width = bottom row of stars
+
+    for i in range(base_width):
+        print(star(i + 1))
+
+pyramid(2)
+pyramid(5)
+pyramid(20)
