4230af931abd26f1232a9f3a6ce66a7b7f7a6af5
Vincent Chen <vincentc0911@gmail.com>
2024-04-09 09:50:26 -0700

    Edit work

--- Work/work-unit-mid2-activity.py
+++ Work/work-unit-mid2-activity.py
@@ -3,3 +3,59 @@
 # 9 April. 2024
 
 
+import random
+import time
+
+
+# def num(start: str) -> int:
+#     value = " "
+#     if start == 0:
+#         print("Sorry I can't count down from zero")
+    
+#     elif start < 0:
+#         print(f"Sorry but I can't count down from negative numbers")
+        
+#     elif start > 0:
+#         int(start)
+#         return start
+
+    
+    
+
+# def countdown(num):
+#     for i in range(start):
+#         print(num(num - 1))
+
+
+    
+# start = input("What number do you want me to count down from?")
+# str(start)
+# num(start)
+
+# countdown(int(start))
+
+
+def int_x:
+
+
+def check(start:str) -> int:
+    int(start)
+    if start == 0:
+        print("Sorry I can't countup from zero")
+    elif start < 0:
+        print("sorry, I can't countup from a negative number")
+    elif start > 0:
+        int(start)
+        begin = start
+        return begin
+    
+def countup(begin: str) -> int:
+    int(begin)
+    for i in range(begin):
+        print(check(i + 1))
+
+
+
+x = input("What number do you want me to count up to?")
+
+check(int_x)
