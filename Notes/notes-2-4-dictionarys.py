72a5370ba4141cfe147fdb9b105b8dfd360982e1
Vincent Chen <vincentc0911@gmail.com>
2024-04-15 13:44:49 -0700

    New Notes

---
+++ Notes/notes-2-4-dictionarys.py
@@ -0,0 +1,13 @@
+# notes - dictionaries
+
+# Big ideas:
+# - A Dictionary is a data sturcture
+# - Dictionaries map keys and values
+
+person_dict = {
+    "name": "John",
+    "age": 24,
+    "CC num": "4500 1023 2222 1111"
+}
+print(person_dict["CC num"])
+print(person_dict["name"])
