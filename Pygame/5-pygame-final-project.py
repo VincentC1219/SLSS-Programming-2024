d0f933305407b1bd187a4965b7fcc79ace960b7d
Vincent Chen <vincentc0911@gmail.com>
2024-06-03 09:49:03 -0700

    Update

--- Pygame/5-pygame-final-project.py
+++ Pygame/5-pygame-final-project.py
@@ -1,4 +1,85 @@
 import random
-import pygame as py
+import pygame as pg
+
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+
+WIDTH = 1280  # Pixels
+HEIGHT = 720
+SCREEN_SIZE = (WIDTH, HEIGHT)
+
+class Cursor(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = self.image = pg.Surface((10,10))
+        self.image.fill(WHITE)
 
 
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Updates the loction of sprite to mouse cursor"""
+        next_pos = pg.mouse.get_pos()
+
+        self.rect.center = next_pos
+
+
+
+def start():
+    """Environment Setup and Game Loop"""
+
+    pg.init()
+    pg.mouse.set_visible(False)
+
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+
+    # All sprites go in this sprite Group
+    all_sprites = pg.sprite.Group()
+
+    cursor = Cursor()
+
+    all_sprites.add(cursor)
+    
+
+    pg.display.set_caption("<WINDOW TITLE HERE>")
+
+    # --Main Loop--
+    while not done:
+        # --- Event Listener
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+
+        # --- Update the world state
+
+        all_sprites.update()
+
+        # --- Draw items
+        screen.fill(BLACK)
+
+        all_sprites.draw(screen)
+
+        # Update the screen with anything new
+        pg.display.flip()
+
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+
+
+def main():
+    start()
+
+
+if __name__ == "__main__":
+    main()
